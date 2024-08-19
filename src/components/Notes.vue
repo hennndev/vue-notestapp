f590f280ce79b9201bc32e5bb0672a72d2e7a1ba
hennndev <hendraputra230897@gmail.com>
2024-08-19 16:47:58 +0700

    (feat) add icon new note

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -10,7 +10,13 @@ import Note from './Note.vue';
 
 <template>
     <section class="mt-16">
-        <h1 class="text-6xl font-medium text-primary tracking-tighter">Notes</h1>
+        <section class="flex-between">
+            <h1 class="text-6xl font-medium text-primary tracking-tighter">Notes</h1>
+            <div class="group bg-primary rounded-full py-3 px-6 flexx space-x-3 cursor-pointer border-2 border-transparent hover:bg-white hover:border-primary transition duration-200 ease-in">
+                <i class="pi pi-plus text-white group-hover:text-primary transition duration-200 ease-in"></i>
+                <p class="text-lg text-white group-hover:text-primary transition duration-200 ease-in">New note</p>
+            </div>
+        </section>
 
         <section class="mt-10 grid grid-cols-notes gap-x-[20px] gap-y-[40px]">
             <Note/>
