e5b6691b551293c3fe415999df786b1ea160db42
hennndev <hendraputra230897@gmail.com>
2024-08-20 02:05:21 +0700

    (feat) fixed title note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -15,17 +15,19 @@
 
 <template>
     <article class="flex-between flex-col rounded-[20px] p-6 min-h-[250px]" :class="bgNote">
-        <p class="text-white font-medium tracking-tight text-xl self-start">
+        <p class="text-primary font-medium tracking-tight text-2xl self-start">
             {{ this.data.title }}
         </p>
-
         <section class="flex-between w-full">
-            <p class="text-white text-lg">{{ date }}</p>
-            <div class="flexx space-x-2">
-                <div class="group bg-primary rounded-full p-3 flex-center cursor-pointer hover:bg-white transition duration-200 ease-in">
-                    <i class="pi pi-bookmark text-sm text-white group-hover:text-primary transition duration-200 ease-in"></i>
+            <p class="text-primary text-lg self-end">{{ date }}</p>
+            <div class="flexx flex-col space-y-3">
+                <div class="group bg-primary rounded-full p-2.5 flex-center cursor-pointer hover:bg-white transition duration-200 ease-in">
+                    <i class="pi pi-plus text-sm text-white group-hover:text-primary transition duration-200 ease-in"></i>
                 </div>
-                <div class="group bg-primary rounded-full p-3 flex-center cursor-pointer hover:bg-white transition duration-200 ease-in">
+                <div class="group bg-primary rounded-full p-2.5 flex-center cursor-pointer hover:bg-white transition duration-200 ease-in">
+                    <i class="pi pi-trash text-sm text-white group-hover:text-primary transition duration-200 ease-in"></i>
+                </div>
+                <div class="group bg-primary rounded-full p-2.5 flex-center cursor-pointer hover:bg-white transition duration-200 ease-in">
                     <i class="pi pi-pencil text-sm text-white group-hover:text-primary transition duration-200 ease-in"></i>
                 </div>
             </div>
