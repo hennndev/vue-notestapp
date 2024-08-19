9b3c77251338cb1cea5b6e74de885ff433e4d717
hennndev <hendraputra230897@gmail.com>
2024-08-19 15:22:41 +0700

    add tailwind css and search input component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <section class="container">
+        <section class="flexx w-full border border-gray-200 rounded-lg px-6">
+            <i class="pi pi-search text-md text-gray-500 mr-4"></i>
+            <input type="text" placeholder="Find notes title" class="flex-1 py-2 text-base outline-none text-gray-400">
+        </section>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
